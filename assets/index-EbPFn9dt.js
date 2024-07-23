fd512666e889a8fb652db1c8700e11f58213a601
jose.ruiz <jose.ruiz@uner.edu.ar>
2024-07-22 21:01:39 -0300

    Updates

--- assets/index-EbPFn9dt.js
+++ assets/index-EbPFn9dt.js
@@ -128,7 +128,7 @@ Error generating stack: `+s.message+`
         <details>
           <summary>Debug info (PLEASE, DO NOT DELETE)</summary>
 
-          **Version**: [${"96d199b"}](https://github.com/vonsim/vonsim/commit/${"96d199b"})
+          **Version**: [${"d049454"}](https://github.com/vonsim/vonsim/commit/${"d049454"})
 
           #### Program
 
@@ -145,32 +145,19 @@ Error generating stack: `+s.message+`
           
         </details>
       `}}},XV=ge.from(ge.MAX_ADDRESS).toString(),qV={generics:{clean:"Limpiar","io-register":(t,e)=>`Registro ${t} (${Kr.format(e)})`,"byte-representation":{hex:"Hexadecimal",bin:"Binario",uint:"Entero sin signo",int:"Entero con signo","safe-ascii":"ASCII"}},update:{"update-available":"¡Hay una nueva versión disponible!",reload:"Actualizar"},messages:{"assemble-error":"Error de ensamblado. Solucione los errores y vuelva a intentar.","invalid-action":"Acción inválida."},editor:{lintSummary:t=>t===0?"Listo para compilar":t===1?"Hay un error":`Hay ${t} errores`,files:{unsupported:"Tu navegador no soporta la “FileSystem API”","no-file":"No hay ningún archivo abierto",open:"Abrir",unsaved:"Hay cambios sin guardar, ¿desea descartarlos?","open-error":"Error al abrir el archivo",save:"Guardar","save-as":"Guardar como","save-error":"Error al guardar el archivo"},example:bg`
-      ; ¡Bienvenido a VonSim!
-      ; Este es un ejemplo de código que calcula los primeros
-      ; n números de la sucesión de Fibonacci, y se guardan a
-      ; partir de la posición 1000h de la memoria.
+      ; ¡Bienvenido a VonSim 8!
+      ; Este es un ejemplo de código que suma dos variables z = x + y
       
-           n  equ 10    ; Calcula los primeros 10 números
-      
-              org 1000h
-      inicio  db 1
-      
-              org 2000h
-              mov bx, offset inicio + 1
-              mov al, 0
-              mov ah, inicio
-      
-      bucle:  cmp bx, offset inicio + n
-              jns fin
-              mov cl, ah
-              add cl, al
-              mov al, ah
-              mov ah, cl
-              mov [bx], cl
-              inc bx
-              jmp bucle
-      fin:    hlt
-              end
+      org 1000h
+        x  db 3
+        y  db 2
+        z  db 0
+      org 2000h
+        mov al, x
+        add al, y
+        mov z, al
+        hlt
+      end
     `},control:{action:{start:"Iniciar",continue:"Continuar",running:"Ejecutando...",run:{"cycle-change":"Un ciclo","end-of-instruction":"Una instrucción",infinity:"Hasta el final"},stop:"Detener"},tabs:{editor:"Editor",computer:"Computadora"},zoom:{in:"Acercar",out:"Alejar"}},computer:{cpu:{name:"CPU",register:t=>`Registro ${t}`,"control-unit":"Unidad de control",decoder:"Decodificador",status:{fetching:"Leyendo instrucción...","fetching-operands":"Leyendo operandos...",executing:"Ejecutando...",writeback:"Escribiendo resultados...",interrupt:"Manejando interrupción...",stopped:"Detenido","stopped-error":"Error","waiting-for-input":"Esperando tecla...",int6:"Ejecutando INT 6...",int7:"Ejecutando INT 7..."}},memory:{name:"Memoria",cell:t=>`Celda ${ge.format(t)}`,"fix-address":"Fijar dirección","unfix-address":"Desfijar dirección","address-must-be-integer":"El valor de inicio debe ser un número entero hexadecimal.","address-out-of-range":`El valor de inicio debe ser menor o igual a ${XV}.`,"address-increment":"Incrementar dirección (Page Up)","address-decrement":"Decrementar dirección (Page Down)"},"chip-select":{name:"Chip select",mem:"mem",pic:"pic",timer:"timer",pio:"pio",handshake:"handshake"},f10:"Tecla F10",keyboard:"Teclado",leds:"Leds",printer:{name:"Impresora",buffer:"Buffer"},screen:"Pantalla",switches:"Llaves",handshake:{name:"Handshake",data:"Dato",state:"Estado"},pic:"PIC",pio:{name:"PIO",port:t=>`Puerto ${t}`},timer:"Timer"},settings:{title:"Configuración",language:{label:"Idioma"},editorFontSize:{label:"Tamaño de fuente del editor",increase:"Aumentar",decrease:"Disminuir"},dataOnLoad:{label:"Memoria al cargar",description:"Qué hacer con la memoria al cargar un nuevo programa.",randomize:"Aleatoria",clean:"Vaciar",unchanged:"Reusar"},devices:{label:"Dispositivos",description:"Qué conjunto de dispositivos usar.","no-devices":"Ninguno","keyboard-and-screen":"Teclado y pantalla","pio-switches-and-leds":"Llaves y luces","pio-printer":"Impresora (PIO)",handshake:"Impresora (Handshake)"},animations:{label:"Animaciones",description:["Inhabilitando las animaciones, se obtiene ejecución más rápida.","Solo afecta a las animaciones afectadas por la velocidad de simulación (como la CPU).","Otras animaciones (como el reloj y la impresora) se ejecutarán a su propia velocidad.","¡Cuidado! Velocidades de simulación muy altas sin animaciones puede saturar el procesador."].join(" ")},speeds:{executionUnit:"Velocidad de simulación",clockSpeed:"Velocidad del reloj",printerSpeed:"Velocidad de impresión"},filters:{label:"Filtros",description:"Filtros aplicados a la página. Útil cuando se usa un proyector.",revert:"Revertir a los valores por defecto",brightness:"Brillo",contrast:"Contraste",invert:"Invertir",saturation:"Saturación"},reset:"Restablecer configuración"},footer:{documentation:"Documentación",copyright:"III-LIDI, FI, UNLP",issue:{report:"Reportar un error",body:(t,e)=>bg`
         <!-- Por favor, describa el problema que está teniendo en la mayor cantidad de detalle posible. -->
         <!-- Por sobre todo, agregue los pasos para reproducir el problema. -->
@@ -178,7 +165,7 @@ Error generating stack: `+s.message+`
         <details>
           <summary>Información extra (POR FAVOR, NO BORRAR)</summary>
 
-          **Versión**: [${"96d199b"}](https://github.com/vonsim/vonsim/commit/${"96d199b"})
+          **Versión**: [${"d049454"}](https://github.com/vonsim/vonsim/commit/${"d049454"})
 
           #### Programa
 
