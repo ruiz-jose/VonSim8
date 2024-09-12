311231b0a9a0699dfc0dff40d59e12c2e77dcf85
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2024-09-12 14:39:29 +0000

    cambio tope de pila sp

--- packages/simulator/src/memory.ts
+++ packages/simulator/src/memory.ts
@@ -33,7 +33,7 @@ export type MemoryOperation =
  * This class is: MUTABLE
  */
 export class Memory extends Component {
-  static readonly SIZE = MemoryAddress.MAX_ADDRESS + 1;
+  static readonly SIZE = MemoryAddress.MAX_ADDRESS;
 
   #buffer: Uint8Array;
   #codeMemory: Set<number>;
