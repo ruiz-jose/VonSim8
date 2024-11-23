6010f4a9a0e4d59c3e676f3d89c0d95cc29124a8
jose-ruiz <jose.ruiz@uner.edu.ar>
2024-11-23 12:08:51 -0300

    elimino warnings de control

--- app/src/components/Controls.tsx
+++ app/src/components/Controls.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { ChevronRight, ChevronsRight, Infinity, StopCircle } from 'lucide-react';
 import { useCallback } from "react";
 import { useKey } from "react-use";
 
@@ -53,41 +54,41 @@ export function Controls({ className }: { className?: string }) {
 
   return (
     <div className={clsx("flex items-center justify-center gap-4", className)}>
-      <button
-        disabled={status.type === "running"}
-        onClick={runCycle}
-        className="bg-green-500 focus-visible:ring-green-500 inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-      >
-        <span className="icon-[lucide--chevron-right] mr-2 h-4 w-4" />
-        {translate("control.action.run.cycle-change")}
-        <kbd className="ml-2 text-stone-600">F7</kbd>
-      </button>
-      <button
-        disabled={status.type === "running"}
-        onClick={runInstruction}
-        className="bg-mantis-500 focus-visible:ring-mantis-500 inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-      >
-        <span className="icon-[lucide--chevrons-right] mr-2 h-4 w-4" />
-        {translate("control.action.run.end-of-instruction")}
-        <kbd className="ml-2 text-stone-600">F8</kbd>
-      </button>
-      <button
-        disabled={status.type === "running"}
-        onClick={runInfinity}
-        className="bg-mantis-500 focus-visible:ring-mantis-500 inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-      >
-        <span className="icon-[lucide--infinity] mr-2 h-4 w-4" />
-        {translate("control.action.run.infinity")}
-        <kbd className="ml-2 text-stone-600">F4</kbd>
-      </button>
-      <button
-        disabled={status.type === "stopped"}
-        onClick={() => dispatch("cpu.stop")}
-        className="bg-mantis-500 focus-visible:ring-mantis-500 inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-      >
-        <span className="icon-[lucide--stop-circle] mr-2 h-4 w-4" />
-        {translate("control.action.stop")}
-      </button>
-    </div>
+    <button
+      disabled={status.type === "running"}
+      onClick={runCycle}
+      className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-green-500 px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    >
+      <ChevronRight className="mr-2 h-4 w-4" />
+      <span className="text-sm font-medium">{translate("control.action.run.cycle-change")}</span>
+      <kbd className="ml-2 text-stone-600">F7</kbd>
+    </button>
+    <button
+      disabled={status.type === "running"}
+      onClick={runInstruction}
+      className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-green-500 px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    >
+      <ChevronsRight className="mr-2 h-4 w-4" />
+      <span className="text-sm font-medium">{translate("control.action.run.end-of-instruction")}</span>
+      <kbd className="ml-2 text-stone-600">F8</kbd>
+    </button>
+    <button
+      disabled={status.type === "running"}
+      onClick={runInfinity}
+      className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-green-500 px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    >
+      <Infinity className="mr-2 h-4 w-4" />
+      <span className="text-sm font-medium">{translate("control.action.run.infinity")}</span>
+      <kbd className="ml-2 text-stone-600">F4</kbd>
+    </button>
+    <button
+      disabled={status.type === "stopped"}
+      onClick={() => dispatch("cpu.stop")}
+      className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-red-500 px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    >
+      <StopCircle className="mr-2 h-4 w-4" />
+      <span className="text-sm font-medium">{translate("control.action.stop")}</span>
+    </button>
+  </div>
   );
 }
