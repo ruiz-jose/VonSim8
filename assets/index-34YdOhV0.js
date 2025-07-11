635870d5142aa5946b4c16ff2f9f26390be361d5
ruiz-jose <ruiz-jose@users.noreply.github.com>
2025-07-11 20:00:44 +0000

    deploy: 32741de5751d8993486ccad49a2d7f029526d632

--- assets/index-34YdOhV0.js
+++ assets/index-34YdOhV0.js
@@ -127,7 +127,7 @@ Error generating stack: `+s.message+`
         <details>
           <summary>Debug info (PLEASE, DO NOT DELETE)</summary>
 
-          **Version**: [${"0e4bb13"}](https://github.com/vonsim/vonsim/commit/${"0e4bb13"})
+          **Version**: [${"32741de"}](https://github.com/vonsim/vonsim/commit/${"32741de"})
 
           #### Program
 
@@ -161,7 +161,7 @@ Error generating stack: `+s.message+`
         <details>
           <summary>Información extra (POR FAVOR, NO BORRAR)</summary>
 
-          **Versión**: [${"0e4bb13"}](https://github.com/ruiz-jose/VonSim8/commit/${"0e4bb13"})
+          **Versión**: [${"32741de"}](https://github.com/ruiz-jose/VonSim8/commit/${"32741de"})
 
           #### Programa
 
