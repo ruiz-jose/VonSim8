0da14b4ff573cefbb8956339de3a46c108dc4f3a
Jose Ruiz <jose.ruiz@uner.edu.ar>
2025-09-02 16:17:45 -0300

    Ajustar el z-index del componente ChipSelect para mejorar la superposición en la interfaz

--- app/src/computer/bus/ChipSelect.tsx
+++ app/src/computer/bus/ChipSelect.tsx
@@ -8,7 +8,7 @@ export function ChipSelect() {
   if (!devices.hasIOBus) return null;
 
   return (
-    <div className="absolute left-[500px] top-[525px] z-10 h-[70px] w-[250px] rounded-lg border border-stone-600 bg-stone-900">
+  <div className="absolute left-[500px] top-[525px] z-40 h-[70px] w-[250px] rounded-lg border border-stone-600 bg-stone-900">
       <span className="block w-min whitespace-nowrap rounded-br-lg rounded-tl-lg border-b border-r border-stone-600 bg-mantis-500 px-2 py-1 text-lg text-white">
         {translate("computer.chip-select.name")}
       </span>
